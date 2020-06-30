f6452e7726c5c0787a1afe631a3504be28fb19ff
Hector Herrera <hectorsum81@hotmail.com>
2020-06-29 23:33:44 -0500

    database file changed, only name

--- Project/src/routes/index.js
+++ Project/src/routes/index.js
@@ -2,7 +2,6 @@ const express=require('express');
 const router=express.Router();
 const {islogedin,isnotlogedin} = require('../lib/out');
 
-
 router.get('/', (req,res)=>{
   //*Aiming to layout dir, index.hbs file
   res.render('index',{title:'index page',layout:'index'});
