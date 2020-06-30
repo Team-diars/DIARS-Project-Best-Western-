30be50e317676262e8316057ce1b315c2699ec59
Hector Herrera <hectorsum81@hotmail.com>
2020-06-30 00:52:39 -0500

    post method added within index

--- Project/src/routes/index.js
+++ Project/src/routes/index.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const router=express.Router();
 const {islogedin,isnotlogedin} = require('../lib/out');
+const pool = require('../database');
 
 router.get('/', (req,res)=>{
   //*Aiming to layout dir, index.hbs file
@@ -8,7 +9,22 @@ router.get('/', (req,res)=>{
   // res.send('HOME')
 });
 
-
+router.post('/',async (req,res)=>{
+  const {id} = req.params;
+  const { NombreCompleto,Email,TipoHabitacion,CantPersonas,NumeroCelular,FechaInicio,FechaSalida } = req.body;
+  const newLink = {
+    NombreCompleto,
+    TipoHabitacion,
+    CantPersonas,
+    NumeroCelular,
+    FechaInicio,
+    FechaSalida
+  }
+  console.log(newLink);
+  pool.query('INSERT INTO reserva set ?',[newLink])
+  req.flash('success','Reservation was made successfully')
+  res.redirect('/')
+})
 
 
 module.exports=router;
