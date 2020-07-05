18e983ce127b8fe0246f8f6338b9818000bddad8
Hector Herrera <hectorsum81@hotmail.com>
2020-07-05 04:17:56 -0500

    query selection updated in reservation/list

--- Project/src/routes/reservation.js
+++ Project/src/routes/reservation.js
@@ -5,7 +5,8 @@ const {islogedin,isnotlogedin} = require('../lib/out');
 
 //Listar
 router.get('/',isnotlogedin,async(req,res)=>{
-  const reservation = await pool.query('SELECT * FROM reservation')
+  const reservation = await pool.query("SELECT reservation.*, t_room.roomtype FROM reservation LEFT JOIN t_room ON reservation.troom_id = t_room.troom_id")
+
   res.render('reservation/list',{reservation})
 })
 
