fa04f07bbc86b2f7222c1eaf3e8e4c8e3a58214d
Hector Herrera <hectorsum81@hotmail.com>
2020-07-06 05:24:30 -0500

    two new columns added in database, one column added in post method(random-ticket)

--- Project/src/routes/index.js
+++ Project/src/routes/index.js
@@ -29,16 +29,30 @@ router.post('/',async (req,res)=>{
 
   //*Its same thing with troom_price, we select first element and price value from it
   const total = troom_price[0].price*peoplequantity;
+  const randomTicket = Math.floor((Math.random()*5)+1);
+  const gettingTicket = await pool.query('SELECT num_ticket FROM reservation');
 
+  // Array.prototype.forEach.call(gettingTicket[0].num_ticket.children,elem =>{
+  //   while (true){
+  //     if(randomTicket!=elem){
+  //       return randomTicket;
+  //     }
+  //     randomTicket = Math.floor((Math.random()*5)+1);
+  //   }
+  // })
+  console.log(gettingTicket); //*Remove RowDataPacket from every iteration
+  gettingTicket.forEach(elem=>{
+    return elem[0];
+  })
   const newLink = {
-    
+    num_ticket: randomTicket,
     fullname,
     phonenumber,
     peoplequantity,
     checkin:validFechaInicio,
     checkout:validFechaSalida,
     total,
-    troom_id:troom //*it returns already the troom's id
+    troom_id:troom, //*it returns already the troom's id
   }
   console.log(newLink);
   pool.query('INSERT INTO reservation set ?',[newLink])
